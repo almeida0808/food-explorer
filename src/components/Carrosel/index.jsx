b91010779cd24a578295f08b4d88e38ee6111be2
almeida0808 <almeidaff08@mail.com>
2024-07-02 13:54:35 -0300

    front end inicialmente pronto

--- src/components/Carrosel/index.jsx
+++ src/components/Carrosel/index.jsx
@@ -2,33 +2,74 @@ import { Container } from "./styles";
 import { CardFood } from "../Card-food";
 import maracuja from "../../assets/maracuja.png";
 import { useMediaQuery } from "react-responsive";
-import { Divide } from "@phosphor-icons/react";
+import { CaretLeft, CaretRight } from "@phosphor-icons/react";
+import { useRef } from "react";
 
-export function CarrosselFood({ title,like= false, isAdmin, ...rest }) {
+export function CarrosselFood({ title, like = false, isAdmin, ...rest }) {
   const isDesktop = useMediaQuery({ minWidth: 768 }); // verifica se tem tamanho de desktop
+  const carrosel = useRef(null);
 
+  const handleLeftClick = (e) => {
+    e.preventDefault();
+    console.log(carrosel.current.offsetWidth);
+    carrosel.current.scrollLeft -= carrosel.current.offsetWidth;
+  };
+  const handleRightClick = (e) => {
+    e.preventDefault();
+    carrosel.current.scrollLeft += carrosel.current.offsetWidth;
+  };
 
   return (
     <Container className="carrosel">
-      
       <h2>{title}</h2>
 
-{isDesktop & (<div></div>)}
-      <section className="PreviewFoods">
-        {/* esse são os cards dos produtods mas pode ter quantos quiser, provavelmente você vai pega=los de um banco de dados */}
-        <CardFood
+      <div className="PreviewFoods">
+        {isDesktop && (
+          <div className="arrows">
+            <div className="left">
+              <button onClick={handleLeftClick}>{<CaretLeft />}</button>
+            </div>
+            <div className="right">
+              <button onClick={handleRightClick}>{<CaretRight />}</button>
+            </div>
+          </div>
+        )}
+        <section className="cards" ref={carrosel}>
+          {/* esse são os cards dos produtods mas pode ter quantos quiser, provavelmente você vai pega=los de um banco de dados */}
+         
+          <CardFood
+            title={"Suco de Maracujá"}
+            like={like}
+            isAdmin={isAdmin}
+            value={"19,90"}
+            imageUrl="https://s3-alpha-sig.figma.com/img/2a3a/f2da/858bcacaf0fdef44b4e289c158179e28?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=J1AFsKIs0nsPxInM5htX71nPs8KSTDe7t11~d9bBKnuWdK0PVBY5RcSDX2uD9rsNYKXpqw9-twHFmSbwq~QxVtgC9SGLBPO5ui5Og6kd2d3ib0xXNnnqF~qtXjTYj5trL-hyPXmPFLDleBpSLckxlpPqiuVVxwqBQ3BABFiR0lO9F9Ed0Si9KUPMwNr84X-4kRxDm46E3j7PW~O5Xs1BmgycR~OCy3D5ECQFv60ytAvXMfHetfGNj5XT5ScQtb2zk4jSn8AS7Xy3P1WeInuJAAK83FL1YbgtGJPjrFkPKmtTxETPnZdD2tFQEC9g4Avnk7u12xc3RFWJZ57KNvq2jg__"
+          />
+          <CardFood
+            title={"Suco de Maracujá"}
+            like={like}
+            isAdmin={isAdmin}
+            value={"19,90"}
+            imageUrl="https://s3-alpha-sig.figma.com/img/3963/9111/6d672c4f830cbb8c6c5536ade350b379?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Rk5ljBezCQcvB7T0CktfqeS-1Qt5PMR0h-w6KwW9EKqF75lJutMJaucIw5BSWngtcCXHRp6DD7fMTWcU3IDQ4Xg9Ua9ehuK2uLoTzGJ-vbhWaUwwfkfo5uQJhGS2LlOGtbM5jlhusSiwn~1vRyN-GiQu-aG1NPJVeXawxizd5jDdpYMVOU9X5p2bMzztsWlQRortiXAad~J06RY4-WCippyz9Q7GF~oGx0nyGpDB5A5sseiCpIPD2hF-wD5oAgM5TrMg0ONipKLpuZyapcKJWkWxp5xfCrZWnL8AslrbYzzb1-grVAA8Br1XsJNtx0UrypKvx7XzHq6G9i2u7hdovA__"
+          />{" "}
+          <CardFood
+            title={"Suco de Maracujá"}
+            like={like}
+            isAdmin={isAdmin}
+            value={"19,90"}
+            imageUrl={maracuja}
+          /> <CardFood
           title={"Suco de Maracujá"}
           like={like}
           isAdmin={isAdmin}
           value={"19,90"}
-          imageUrl={maracuja}
+          imageUrl="https://s3-alpha-sig.figma.com/img/2a3a/f2da/858bcacaf0fdef44b4e289c158179e28?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=J1AFsKIs0nsPxInM5htX71nPs8KSTDe7t11~d9bBKnuWdK0PVBY5RcSDX2uD9rsNYKXpqw9-twHFmSbwq~QxVtgC9SGLBPO5ui5Og6kd2d3ib0xXNnnqF~qtXjTYj5trL-hyPXmPFLDleBpSLckxlpPqiuVVxwqBQ3BABFiR0lO9F9Ed0Si9KUPMwNr84X-4kRxDm46E3j7PW~O5Xs1BmgycR~OCy3D5ECQFv60ytAvXMfHetfGNj5XT5ScQtb2zk4jSn8AS7Xy3P1WeInuJAAK83FL1YbgtGJPjrFkPKmtTxETPnZdD2tFQEC9g4Avnk7u12xc3RFWJZ57KNvq2jg__"
         />
         <CardFood
           title={"Suco de Maracujá"}
           like={like}
           isAdmin={isAdmin}
           value={"19,90"}
-          imageUrl={maracuja}
+          imageUrl="https://s3-alpha-sig.figma.com/img/3963/9111/6d672c4f830cbb8c6c5536ade350b379?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Rk5ljBezCQcvB7T0CktfqeS-1Qt5PMR0h-w6KwW9EKqF75lJutMJaucIw5BSWngtcCXHRp6DD7fMTWcU3IDQ4Xg9Ua9ehuK2uLoTzGJ-vbhWaUwwfkfo5uQJhGS2LlOGtbM5jlhusSiwn~1vRyN-GiQu-aG1NPJVeXawxizd5jDdpYMVOU9X5p2bMzztsWlQRortiXAad~J06RY4-WCippyz9Q7GF~oGx0nyGpDB5A5sseiCpIPD2hF-wD5oAgM5TrMg0ONipKLpuZyapcKJWkWxp5xfCrZWnL8AslrbYzzb1-grVAA8Br1XsJNtx0UrypKvx7XzHq6G9i2u7hdovA__"
         />{" "}
         <CardFood
           title={"Suco de Maracujá"}
@@ -36,27 +77,19 @@ export function CarrosselFood({ title,like= false, isAdmin, ...rest }) {
           isAdmin={isAdmin}
           value={"19,90"}
           imageUrl={maracuja}
-        />
-          <CardFood
-          
-          title={"Suco de Maracujá"}
-          like={like}
-          isAdmin={isAdmin}
-          value={"19,90"}
-          imageUrl={maracuja}
-        />  <CardFood
+        /> <CardFood
         title={"Suco de Maracujá"}
         like={like}
         isAdmin={isAdmin}
         value={"19,90"}
-        imageUrl={maracuja}
+        imageUrl="https://s3-alpha-sig.figma.com/img/2a3a/f2da/858bcacaf0fdef44b4e289c158179e28?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=J1AFsKIs0nsPxInM5htX71nPs8KSTDe7t11~d9bBKnuWdK0PVBY5RcSDX2uD9rsNYKXpqw9-twHFmSbwq~QxVtgC9SGLBPO5ui5Og6kd2d3ib0xXNnnqF~qtXjTYj5trL-hyPXmPFLDleBpSLckxlpPqiuVVxwqBQ3BABFiR0lO9F9Ed0Si9KUPMwNr84X-4kRxDm46E3j7PW~O5Xs1BmgycR~OCy3D5ECQFv60ytAvXMfHetfGNj5XT5ScQtb2zk4jSn8AS7Xy3P1WeInuJAAK83FL1YbgtGJPjrFkPKmtTxETPnZdD2tFQEC9g4Avnk7u12xc3RFWJZ57KNvq2jg__"
       />
       <CardFood
         title={"Suco de Maracujá"}
         like={like}
         isAdmin={isAdmin}
         value={"19,90"}
-        imageUrl={maracuja}
+        imageUrl="https://s3-alpha-sig.figma.com/img/3963/9111/6d672c4f830cbb8c6c5536ade350b379?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Rk5ljBezCQcvB7T0CktfqeS-1Qt5PMR0h-w6KwW9EKqF75lJutMJaucIw5BSWngtcCXHRp6DD7fMTWcU3IDQ4Xg9Ua9ehuK2uLoTzGJ-vbhWaUwwfkfo5uQJhGS2LlOGtbM5jlhusSiwn~1vRyN-GiQu-aG1NPJVeXawxizd5jDdpYMVOU9X5p2bMzztsWlQRortiXAad~J06RY4-WCippyz9Q7GF~oGx0nyGpDB5A5sseiCpIPD2hF-wD5oAgM5TrMg0ONipKLpuZyapcKJWkWxp5xfCrZWnL8AslrbYzzb1-grVAA8Br1XsJNtx0UrypKvx7XzHq6G9i2u7hdovA__"
       />{" "}
       <CardFood
         title={"Suco de Maracujá"}
@@ -64,15 +97,29 @@ export function CarrosselFood({ title,like= false, isAdmin, ...rest }) {
         isAdmin={isAdmin}
         value={"19,90"}
         imageUrl={maracuja}
-      />
-        <CardFood
-        title={"Suco de Maracujá"}
-        like={like}
-        isAdmin={isAdmin}
-        value={"19,90"}
-        imageUrl={maracuja}
-      />
-      </section>
+      /> <CardFood
+      title={"Suco de Maracujá"}
+      like={like}
+      isAdmin={isAdmin}
+      value={"19,90"}
+      imageUrl="https://s3-alpha-sig.figma.com/img/2a3a/f2da/858bcacaf0fdef44b4e289c158179e28?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=J1AFsKIs0nsPxInM5htX71nPs8KSTDe7t11~d9bBKnuWdK0PVBY5RcSDX2uD9rsNYKXpqw9-twHFmSbwq~QxVtgC9SGLBPO5ui5Og6kd2d3ib0xXNnnqF~qtXjTYj5trL-hyPXmPFLDleBpSLckxlpPqiuVVxwqBQ3BABFiR0lO9F9Ed0Si9KUPMwNr84X-4kRxDm46E3j7PW~O5Xs1BmgycR~OCy3D5ECQFv60ytAvXMfHetfGNj5XT5ScQtb2zk4jSn8AS7Xy3P1WeInuJAAK83FL1YbgtGJPjrFkPKmtTxETPnZdD2tFQEC9g4Avnk7u12xc3RFWJZ57KNvq2jg__"
+    />
+    <CardFood
+      title={"Suco de Maracujá"}
+      like={like}
+      isAdmin={isAdmin}
+      value={"19,90"}
+      imageUrl="https://s3-alpha-sig.figma.com/img/3963/9111/6d672c4f830cbb8c6c5536ade350b379?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Rk5ljBezCQcvB7T0CktfqeS-1Qt5PMR0h-w6KwW9EKqF75lJutMJaucIw5BSWngtcCXHRp6DD7fMTWcU3IDQ4Xg9Ua9ehuK2uLoTzGJ-vbhWaUwwfkfo5uQJhGS2LlOGtbM5jlhusSiwn~1vRyN-GiQu-aG1NPJVeXawxizd5jDdpYMVOU9X5p2bMzztsWlQRortiXAad~J06RY4-WCippyz9Q7GF~oGx0nyGpDB5A5sseiCpIPD2hF-wD5oAgM5TrMg0ONipKLpuZyapcKJWkWxp5xfCrZWnL8AslrbYzzb1-grVAA8Br1XsJNtx0UrypKvx7XzHq6G9i2u7hdovA__"
+    />{" "}
+    <CardFood
+      title={"Suco de Maracujá"}
+      like={like}
+      isAdmin={isAdmin}
+      value={"19,90"}
+      imageUrl={maracuja}
+    />
+        </section>
+      </div>
     </Container>
   );
 }
--- src/components/Carrosel/styles.js
+++ src/components/Carrosel/styles.js
@@ -12,13 +12,14 @@ export const Container = styled.div`
     font-weight: 400;
   }
 
-  > .PreviewFoods {
+  .cards {
     // ESTILO DA DIV QUE GUARDA OS CARDS
     display: flex;
     flex-direction: row; // faz com qu os cards fiquem tudo em uma linha só
-    overflow-x: auto; // caso tenha mais de um item ele ativa o scroll , x por causa de ser na vertical
+    overflow-x: scroll; // caso tenha mais de um item ele ativa o scroll , x por causa de ser na vertical
     scroll-behavior: smooth; // deixa o  scroll mais suave
     gap: 1rem; // distancia de um card pro outro
+    position: relative;
 
     &::-webkit-scrollbar {
       display: none;
@@ -29,6 +30,65 @@ export const Container = styled.div`
     h2 {
       font-size: 24px;
       font-weight: 600;
+    }
+
+    .arrows {
+      display: flex;
+      justify-content: space-between;
+      margin: 0 auto;
+      position: absolute;
+      width: 91%;
+
+      button {
+        border: none;
+        display: flex;
+        font-size: 50px;
+        color: white;
+        align-items: center;
+        z-index: 1;
+        background: none;
+      }
+
+      .left,
+      .right {
+        width: 230px;
+        height: 413px;
+        display: flex;
+        z-index: 1;
+      }
+
+      .left {
+        background: linear-gradient(
+          270deg,
+          rgba(9, 9, 121, 0) 0%,
+          rgba(0, 0, 0, 0.847887323943662) 67%
+        );
+        margin-left: -2px;
+      }
+
+      .right {
+        display: flex;
+        justify-content: right;
+        background: linear-gradient(
+          90deg,
+          rgba(9, 9, 121, 0) 0%,
+          rgba(0, 0, 0, 0.847887323943662) 67%
+        );
+
+        button {
+          justify-content: right;
+        }
+      }
+    }
+  }
+
+  @media (min-width: 1024px) {
+    .arrows {
+      width: 90.5%;
+      .left,
+      .right {
+        height: 497px;
+      }
     }
   }
 `;
